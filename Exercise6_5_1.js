c2dc27c4320bb7b0e7310bd56c0d2fdf74817fbb
Chawintee <Chawinteetea@gmail.com>
2020-02-17 15:46:11 +0700

    Exercise6_5_1

---
+++ Exercise6_5_1.js
@@ -0,0 +1,22 @@
+let age=prompt("How old are you?");
+let price 
+
+price = (age<18) ? 2000 : 3500 ;
+
+alert(price);
+console.log(price);
+
+
+
+// if (age<18) 
+// {
+//     price = 2000;
+//     alert(price);
+//     console.log(price);
+// }
+// else 
+// {
+//     price = 3500;
+//     alert(price);
+//     console.log(price);
+// }
