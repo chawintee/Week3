1fc42be910dda1d604525bb8057a8ae3980f5085
Chawintee <Chawinteetea@gmail.com>
2020-02-18 10:55:42 +0700

    add exercise1.9

---
+++ exercise1_9.js
@@ -0,0 +1,10 @@
+let yourAge = prompt(`How old are you`,12);
+
+if (yourAge>=18 && yourAge <=60)
+{
+    alert(`Your age is ${yourAge} \nYour age is between 18 to 60.`);
+}
+else
+{
+    alert(`your age is ${yourAge} \nYour age isn't between 18 to 60.`);
+}
