fed0d9271c61b15773a284fe6aa2cc1112475581
Chawintee <Chawinteetea@gmail.com>
2020-02-18 11:34:25 +0700

    password check

---
+++ exercise1_9_4.js
@@ -0,0 +1,16 @@
+// if(-1||0) // run
+// {
+//     alert(`first`);
+// }
+
+// if(-1 && 0) //not run
+// {
+//     alert(`second`);
+// }
+
+
+
+// if(null || -1 && 1)  //run
+// {
+//     alert(`third`)
+// }
---
+++ exercise1_9_5.js
@@ -0,0 +1,25 @@
+let myUser = prompt(`How are you ???` );
+console.log(myUser);
+if (myUser==`Admin`)
+{
+    let myPassword = prompt('Input your password');
+    if(myPassword==`codecamp#5`)
+    {
+        alert(`Your welcome`);
+        console.log(myPassword);
+    }
+    else if(myPassword==` `||myPassword==null)
+    {
+        alert(`Cancel`);
+        console.log(myPassword);
+    }
+    else
+    {
+        alert(`Wrong password`);
+        console.log(myPassword);
+    }
+}
+else 
+{
+    alert(`I don't know how are you`);
+}
