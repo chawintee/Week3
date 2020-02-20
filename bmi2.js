858ae60838dbdd5da5ab6abd5ad8f9d77117afe4
Chawintee <Chawinteetea@gmail.com>
2020-02-20 15:31:47 +0700

    add bmi2

---
+++ bmi2.js
@@ -0,0 +1,38 @@
+function bmi (kg, m) {
+    let bmi0;
+    bmi0 = kg/(m*m);
+    return bmi0;
+}
+
+function compare(n1, w1, h1, n2, w2 ,h2 ) {
+    let bmi1=bmi(w1, h1), bmi2=bmi(w2, h2);
+    // console.log(bmi1);
+    // console.log(bmi2);
+    if(bmi1 > bmi2){
+        alert(`${n1} ${bmi1}`);
+    }else {
+        alert(`${n2} ${bmi2}`);
+    }
+}
+
+
+// function compare(none, ntwo, bone, btwo){
+//     if(bone > btwo) {
+//         alert(`${none} ${bone}`);
+//     }else {
+//         alert(`${ntwo} ${btwo}`);
+//     }
+// }
+
+
+let myFirstName, myFirstWeight, myFirstHeight, mySecondName, mySecondWeight, mySecondHeight;
+myFirstName = prompt(`put your name 1st`);
+myFirstWeight = +prompt(`put your weight 1st`);
+myFirstHeight = +prompt(`put your height 1st`);
+mySecondName = prompt(`put your name 2nd`);
+mySecondWeight = +prompt(`put your weight 2nd`);
+mySecondHeight = +prompt(`put your height 2nd`);
+
+compare(myFirstName, myFirstWeight, myFirstHeight, mySecondName, mySecondWeight, mySecondHeight)
+
+// compare(myFirstName, mySecondName, myFirstBmi, mySecondBmi);
